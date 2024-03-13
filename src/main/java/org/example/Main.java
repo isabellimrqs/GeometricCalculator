4ca3d859df69d4afdc878a246578a4f85a68fe40
Isabelli Maciel Marques <44590917807@>
2024-03-13 10:11:14 -0300

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String nome;
+
+        System.out.println("Digite seu nome: ");
+        nome = scanner.nextLine();
+        System.out.println("Olá " + nome + "! Seja bem vindo(a)! :D");
+
+        System.out.println("Hoje vamos calcular área e perímetro de algumas formas geométricas...");
+
+
+        while(true){
+
+        }
     }
 }
